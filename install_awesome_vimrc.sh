6033977a4fb713bef3ef0153352a01435770bd41
Nantiwat Thongseng <nwatth@gmail.com>
2015-11-04 19:45:28 +0700

    add my vim

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -1,8 +1,10 @@
 cd ~/.vim_runtime
+echo 'set runtimepath+=~/.vim_runtime' > ~/.vimrc
 
-echo 'set runtimepath+=~/.vim_runtime
+cat ~/.vim_runtime/vimrcs/basic.vim >> ~/.vimrc
 
-source ~/.vim_runtime/vimrcs/basic.vim
+echo '
+
 source ~/.vim_runtime/vimrcs/filetypes.vim
 source ~/.vim_runtime/vimrcs/plugins_config.vim
 source ~/.vim_runtime/vimrcs/extended.vim
@@ -10,6 +12,6 @@ source ~/.vim_runtime/vimrcs/extended.vim
 try
 source ~/.vim_runtime/my_configs.vim
 catch
-endtry' > ~/.vimrc
+endtry' >> ~/.vimrc
 
 echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
